756b6d0f6b3b3370a25dfaa52192e143692135ee
vladhardetskyy@gmail.com <13238274Vv>
2025-05-13 19:22:44 +0300

    Create CategoryEntity

---
+++ WebApiPizushi/Data/Enteties/CategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiPizushi.Data.Enteties;
+
+public class CategoryEntity : BaseEntity<long>
+{
+    [StringLength(250)]
+    public string Name { get; set; } = string.Empty;
+    
+    [StringLength(200)]
+    public string Image { get; set; } = string.Empty;
+
+    [StringLength(250)]
+    public string Slug { get; set; } = string.Empty;
+}
