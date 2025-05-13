9a2626ac5f051a3d93be7a09e4e32f49ae5aa5d8
vladhardetskyy@gmail.com <13238274Vv>
2025-05-13 20:54:37 +0300

    Good working app get list categories

--- WebApiPizushi/Mapper/CategoryMapper.cs
+++ WebApiPizushi/Mapper/CategoryMapper.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using WebApiPizushi.Data.Enteties;
+using WebApiPizushi.Models.Category;
 using WebApiPizushi.Models.Seeder;
 
 namespace WebApiPizushi.Mapper;
@@ -9,5 +10,7 @@ public class CategoryMapper : Profile
     public CategoryMapper()
     {
         CreateMap<SeederCategoryModel, CategoryEntity>();
+
+        CreateMap<CategoryEntity, CategoryItemModel>();
     }
 }
