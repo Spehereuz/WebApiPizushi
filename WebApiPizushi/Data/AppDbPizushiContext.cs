cc0218edce0a10a077d5bc0c5ceba41ca0366194
vladhardetskyy@gmail.com <13238274Vv>
2025-06-03 18:54:20 +0300

    Is good working Identity with seeder

--- WebApiPizushi/Data/AppDbPizushiContext.cs
+++ WebApiPizushi/Data/AppDbPizushiContext.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebApiPizushi.Data.Enteties;
+using WebApiPizushi.Data.Entities.Identity;
 
 namespace WebApiPizushi.Data;
 
-public class AppDbPizushiContext : DbContext
+public class AppDbPizushiContext : IdentityDbContext<UserEntity, RoleEntity, long>
 {
     public AppDbPizushiContext(DbContextOptions<AppDbPizushiContext> opt) : base(opt) { }
 
     public DbSet<CategoryEntity> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<UserRoleEntity>(ur =>
+        {
+            ur.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(r => r.RoleId)
+                .IsRequired();
+
+            ur.HasOne(ur => ur.User)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(u => u.UserId)
+                .IsRequired();
+        });
+    }
 }
--- WebApiPizushi/Data/DbSeeder.cs
+++ WebApiPizushi/Data/DbSeeder.cs
@@ -1,7 +1,10 @@
 ﻿using System.Text.Json;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using WebApiPizushi.Constants;
 using WebApiPizushi.Data.Enteties;
+using WebApiPizushi.Data.Entities.Identity;
 using WebApiPizushi.Interfaces;
 using WebApiPizushi.Models.Seeder;
 
@@ -14,6 +17,8 @@ public static class DbSeeder
         using var scope = webApplication.Services.CreateScope();
         //Цей об'єкт буде верта посилання на конткетс, який зараєстрвоано в Progran.cs
         var context = scope.ServiceProvider.GetRequiredService<AppDbPizushiContext>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
         var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
 
         context.Database.Migrate();
@@ -50,6 +55,62 @@ public static class DbSeeder
             }
         }
 
+        if (!context.Roles.Any())
+        {
+            foreach (var roleName in Roles.AllRoles)
+            {
+                var result = await roleManager.CreateAsync(new(roleName));
+                if (!result.Succeeded)
+                {
+                    Console.WriteLine("Error Create Role {0}", roleName);
+                }
+            }
+        }
 
+        if (!context.Users.Any())
+        {
+            var imageService = scope.ServiceProvider.GetRequiredService<IImageService>();
+            var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "JsonData", "Users.json");
+            if (File.Exists(jsonFile))
+            {
+                var jsonData = await File.ReadAllTextAsync(jsonFile);
+                try
+                {
+                    var users = JsonSerializer.Deserialize<List<SeederUserModel>>(jsonData);
+                    foreach (var user in users)
+                    {
+                        var entity = mapper.Map<UserEntity>(user);
+                        entity.UserName = user.Email;
+                        entity.Image = await imageService.SaveImageFromUrlAsync(user.Image);
+                        var result = await userManager.CreateAsync(entity, user.Password);
+                        if (!result.Succeeded)
+                        {
+                            Console.WriteLine("Error Create User {0}", user.Email);
+                            continue;
+                        }
+                        foreach (var role in user.Roles)
+                        {
+                            if (await roleManager.RoleExistsAsync(role))
+                            {
+                                await userManager.AddToRoleAsync(entity, role);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Not Found Role {0}", role);
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Json Parse Data {0}", ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not Found File Users.json");
+            }
+        }
     }
 }
